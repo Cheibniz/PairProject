cf98ff3a51704cb314db924db3b91822e7a91670
chaibo <17373106@buaa.edu.cn>
2020-03-24 12:37:06 +0800

    封装命令行程序

---
+++ src/Command/intersect.cpp
@@ -0,0 +1,40 @@
+﻿
+#include "Interface.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(int argc, char** argv)
+{
+
+	try
+	{
+		string infile;
+		ofstream outfile;
+		int n;
+		if (!strcmp(argv[1], "-i") && !strcmp(argv[3], "-o"))
+		{
+			infile = argv[2];
+			outfile = ofstream(argv[4]);
+		}
+		else if (!strcmp(argv[3], "-i") && !strcmp(argv[1], "-o"))
+		{
+			infile = argv[4];
+			outfile = ofstream(argv[2]);
+		}
+		else
+		{
+			exit(0);
+		}
+		readFile("in.txt");
+		pair<vector<string>, vector<Point>> result;
+		result = getResult();
+		//cout << result.second.size() << endl;
+		outfile << result.second.size() << endl;
+	}
+	catch (const std::exception&)
+	{
+		
+	}
+	return 0;
+}
+
